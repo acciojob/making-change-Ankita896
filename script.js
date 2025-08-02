c1212fa85eddf52502e0f9cbe8ad1c54da25b558
Ankita896 <ankita143ankyy@gmail.com>
2025-08-02 09:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
 const makeChange = (c) => {
- function makeChange(amount) {
-  let change = {
-    q: 0, 
-    d: 0,
-    n: 0,
-    p: 0  
-  };
+  // your name here
+	 c = parseInt(c);
+    let change = {
+        q: Math.floor(c / 25),
+        d: Math.floor((c % 25) / 10),
+        n: Math.floor(((c % 25) % 10) / 5),
+        p: ((c % 25) % 10) % 5
+    };
+    return change;
 
-  // Calculate quarters
-  change.q = Math.floor(amount / 25);
-  amount %= 25;
+};
 
-  // Calculate dimes
-  change.d = Math.floor(amount / 10);
-  amount %= 10;
-
-  // Calculate nickels
-  change.n = Math.floor(amount / 5);
-  amount %= 5;
-
-  // Calculate pennies
-  change.p = amount;
-
-  return change;
-}
-
-}
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
